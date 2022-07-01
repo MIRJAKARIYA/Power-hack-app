f26dcc2a4a227502b256d75d5653865e9623a744
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-07-01 16:36:51 +0600

    search completed

--- src/components/PowerHack/AllBilling.js
+++ src/components/PowerHack/AllBilling.js
@@ -69,13 +69,17 @@ const AllBilling = () => {
         <div className="flex items-center justify-between mt-10 rounded-md py-2 px-4 mb-5 bg-gray-300">
           <div className="flex-1 flex items-center gap-8">
             <p className="text-lg font-semibold">Billing</p>
-            <div>
+            <div className="relative">
               <input
                 type="text"
                 className="input input-bordered input-error h-[40px] w-[250px]"
                 placeholder="Search"
                 onChange={handleSearch}
+                value={searchText}
               />
+              {
+                searchText && <button onClick={()=>setSearchText('')} className="absolute top-2 right-2 font-bold text-orange-700">X</button>
+              }
             </div>
           </div>
           <div className="flex-1 text-right">
